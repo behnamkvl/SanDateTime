7b4f63ac19036c8a04cc755f020e9e3a49d5167b
Behnam <behnamkvl@gmail.com>
2021-01-09 13:15:46 +0330

    editting setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'sandateTime',         # How you named your package folder (MyLib)
-  packages = ['sandateTime'],   # Chose the same as "name"
+  name = 'sandatetime',         # How you named your package folder (MyLib)
+  packages = ['sandatetime'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This is a repository for managing Persian date-time for our group',   # Give a short description about your library
