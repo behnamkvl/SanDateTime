10b9e5a67db14f315dea4b83cc96ad8728d1515c
Behnam <behnamkvl@gmail.com>
2021-01-09 15:45:09 +0330

    adding gregorian to jalali

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'sandatetime',         # How you named your package folder (MyLib)
   packages = ['sandatetime'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This is a repository for managing Persian date-time for our group',   # Give a short description about your library
   author = 'Behnam Vakili',                   # Type in your name
